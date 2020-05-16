29e579219b6ff3c3b690e1ca00d0533ed11c627b
santy_naren <santhoshnarendra@gmail.com>
2020-05-16 12:43:12 +0530

    menu check

--- src/components/header.js
+++ src/components/header.js
@@ -26,7 +26,7 @@ const Header = ({siteTitle}) => (
 
 
         <a className="navbar-icon" onClick={mobileMenu}>
-           Menu
+        <Link to="/ContactUs">Contact</Link>
         </a>
     </div>
 
